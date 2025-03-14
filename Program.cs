20bea647353895c7005b9486d71e87c5780b5072
jay-tell3 <jaybroww@gmail.com>
2025-03-14 13:49:46 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace mothods_brown_jaylen
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace mothods_brown_jaylen
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter whole number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter anouther whole number");
+            int y = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine(Multiply(x,y));
+
+            Console.WriteLine("What u name is?");
+            string name = Console.ReadLine();
+
+            WelcomeUser(name);
+            goodbye();
         }
-    }
+        static int Multiply(int num1, int num2)
+        { 
+        return num1 * num2; 
+        }
+        static void WelcomeUser(string username)
+        {
+            Console.WriteLine($"Hello, {username}!");
+        }
+        static void goodbye()
+        {
+            Console.WriteLine("have a good one");
+        }
+
+    }  
 }
